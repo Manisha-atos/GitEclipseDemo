423cb03cf6874835c15c1805a52dc99ec53c9e21
Manisha <pimplemp@refiffmail.com>
2024-04-09 17:37:15 +0530

    Test Second commit

--- src/com/p1/Test1.java
+++ src/com/p1/Test1.java
@@ -4,7 +4,12 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello..");
+		new Test1().display();
 
+	}
+	void display()
+	{
+		System.out.println("display");
 	}
 
 }
